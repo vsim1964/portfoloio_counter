d4ef26f7652bae0f7ea2cfecb51cbbb0d3537fb3
Vladimir Siminenko <73741907+vsim1964@users.noreply.github.com>
2023-01-11 19:32:44 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+import Counter from './modules/counter.js';
+
+const counterElements = document.querySelectorAll(`.counter`);
+
+
+counterElements.forEach((element) => {
+  const counter = new Counter(element);
+  counter.init();
+});
